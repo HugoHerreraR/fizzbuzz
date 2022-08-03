9cc076911139ad95e9420a79e4bf02a9486fe759
HugoHerreraH <launchx04415@innovaccion.mx>
2022-08-03 16:27:41 -0500

    Refactorizacion de ExplorerService

--- index.js
+++ index.js
@@ -4,4 +4,6 @@ const ExplorerServices = require("./lib/services/ExplorerService")
 const explorers = Reader.readJsonFile("explorers.json")
 console.log(explorers);
 
-console.log("filtro", ExplorerServices.filterByMission(explorers, "node"));
+console.log("---------> filtro", ExplorerServices.filterByMission(explorers, "node"));
+console.log("---------> Mission", ExplorerServices.getAmountOfExplorersByMission(explorers, "node"));
+console.log("---------> User",  ExplorerServices.getExplorersUsernamesByMission(explorers, "node"));
