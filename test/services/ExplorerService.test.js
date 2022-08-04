644a74e24cb5078292cbb0e2809f21df49ee732d
HugoHerreraH <launchx04415@innovaccion.mx>
2022-08-03 22:46:55 -0500

    Test ExplorerSevice: getExplorersUsernamesByMission

--- test/services/ExplorerService.test.js
+++ test/services/ExplorerService.test.js
@@ -14,4 +14,19 @@ describe("Test para ExplorerService", () => {
         const explorer = ExplorerService.getAmountOfExplorersByMission(explorers, "node");
         expect(explorer).toBe(2);
     })
+});
+
+describe("Test para ExplorerService", () => {
+    test("Requerimiento 3: Obtiene los users en base a una mission", () => {
+        const explorers = [{mission: "node", githubUsername: "Rosa"}, {mission: "node", githubUsername: "Hugo"}, {mission: "java", githubUsername: "Manuel"}];
+        const explorerUser = ExplorerService.getExplorersUsernamesByMission(explorers, "node")
+        expect(explorerUser).toStrictEqual(['Rosa', "Hugo"]);
+        expect(explorerUser.length).toBe(2);
+    });
+    test("Requerimiento 3.1: Obtiene los users en base a una mission", () => {
+        const explorers = [{mission: "node", githubUsername: "Rosa"}, {mission: "node", githubUsername: "Hugo"}, {mission: "java", githubUsername: "Manuel"}];
+        const explorerUser = ExplorerService.getExplorersUsernamesByMission(explorers, "java")
+        expect(explorerUser).toStrictEqual(['Manuel']);
+        expect(explorerUser.length).toBe(1);
+    })
 })
