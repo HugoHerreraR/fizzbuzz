391f467896fd285da8be3c1938649db44bb19e0a
HugoHerreraH <launchx04415@innovaccion.mx>
2022-08-06 20:35:29 -0500

    Test 2: getExplorersUsernamesByMission de explorerController

--- test/controllers/ExplorerController.test.js
+++ test/controllers/ExplorerController.test.js
@@ -1,6 +1,6 @@
 const ExplorerController = require("../../lib/controllers/ExplorerController");
 
-describe("est para ExplorerController", () => {
+describe("Test para ExplorerController", () => {
     test("Test 1: metodo getExplorersByMission", () => {
         const explorersByMission = ExplorerController.getExplorersByMission("node");
         explorersByMission.forEach(function(explorer) {
@@ -12,5 +12,11 @@ describe("est para ExplorerController", () => {
         explorersByMission.forEach(function(explorer) {
             expect(explorer).toHaveProperty('mission', 'java')
         })
+    });
+
+    test("Test 2: Metodo getExplorersUsernamesByMission", () => {
+        const explorerUsername = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(explorerUsername).toEqual(["ajolonauta1", "ajolonauta2","ajolonauta3", "ajolonauta4", "ajolonauta5",
+        "ajolonauta11", "ajolonauta12", "ajolonauta13", "ajolonauta14", "ajolonauta15"])
     })
 })
